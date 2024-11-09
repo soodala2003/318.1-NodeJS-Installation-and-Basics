40e995f5c03198ef0480789440934c6c674216a7
soodala2003 <soodala78@gmail.com>
2024-11-09 02:17:21 -0500

    updated index.js file

--- index.js
+++ index.js
@@ -9,23 +9,8 @@ const routes = {
     "/about": "About Page",
     "/contact": "Contact Page",
 };
-  
-
-/* const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'test/plain');
-    res.end('Hello World!\n');
-}); */
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'test/html');
-    res.write('<h1 style="color: red">Hello World!</h1>');
-    res.write('<p>I wonder what else we can send...</p>');
-    res.end();
-});  
-
-/* const server = http.createServer((req, res) => {
     const response = routes[req.url];
 
     if (response) {
@@ -37,11 +22,44 @@ const server = http.createServer((req, res) => {
         res.write('404 not found');
     } 
     res.end();
-});   */
+});   
 
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+/* http
+    .createServer((req, res) => {
+        const response = routes[req.url];
+
+        if (response) {
+            res.statusCode = 200;
+            es.setHeader('Content-Type', 'test/html');
+            res.write(response);
+        } else {
+            res.statusCode = 404;
+            res.write('404 not found');
+        } 
+        res.end();
+    }).listen(3000, () => {
+        console.log(`Server running.`);
+}); */
+
+/* const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'test/html');
+    res.write('<h1 style="color: red">Hello World!</h1>');
+    res.write('<p>I wonder what else we can send...</p>');
+    res.end();
+});   */
+
+/* const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'test/plain');
+    res.end('Hello World!\n');
+}); */
+
+
 
 
 
